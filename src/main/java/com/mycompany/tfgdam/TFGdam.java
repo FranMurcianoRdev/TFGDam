40121d1935cdd5c01ec7ce4e6f34c3259d319a32
FranDev <franmurcianor.dev@gmail.com>
2024-04-01 11:56:06 +0200

    01.04.24 Parte 2 He puesto en LogicaEmpleados toda la funcionalidad de FormEmpleados. También he hecho Form Proveedores

--- src/main/java/com/mycompany/tfgdam/TFGdam.java
+++ src/main/java/com/mycompany/tfgdam/TFGdam.java
@@ -2,25 +2,17 @@
 
 package com.mycompany.tfgdam;
 
-import igu.FormEmpleados;
-import igu.FormLogin;
-import java.sql.Connection;
-import logica.Conexion;
 
-/**
- *
- * @author franc
- */
+import igu.FormLogin;
+
+
 public class TFGdam {
 
     public static void main(String[] args) {
-        /*FormLogin objLogin = new FormLogin();
+        FormLogin objLogin = new FormLogin();
         objLogin.setVisible(true);
         //Para que la ventana siempre aparezca en el medio: 
         objLogin.setLocationRelativeTo(null);
-        */
-        FormEmpleados obj = new FormEmpleados();
-        obj.setVisible(true);
-        
+    
     }
 }
